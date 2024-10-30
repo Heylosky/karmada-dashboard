e5f8a4ee48f0cf9730635b4c1002c8da4e87df76
Heylosky <2547226479@qq.com>
2024-10-30 18:36:50 +0800

    aaa

---
+++ build.sh
@@ -0,0 +1,6 @@
+#！/bin/bash
+rm -rf ./cluster/images/._*
+rm -rf ./cluster/images/.D*
+make image-karmada-dashboard-api GOARCH=amd64
+docker tag docker.io/karmada/karmada-dashboard-api:node-detail dockerhub.kubekey.local/karmada/karmada-dashboard-api:node-detail
+docker push dockerhub.kubekey.local/karmada/karmada-dashboard-api:node-detail

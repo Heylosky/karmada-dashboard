a3ac7464eefd6b63a5f6ff3bbfd6b106b67fc861
Heylosky <2547226479@qq.com>
2024-11-14 16:11:20 +0800

    change harbor port

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 rm -rf ./cluster/images/._*
 rm -rf ./cluster/images/.D*
 make image-karmada-dashboard-api GOARCH=amd64
-docker tag docker.io/karmada/karmada-dashboard-api:membercluster-node-status dockerhub.kubekey.local/karmada/karmada-dashboard-api:node-detail
-docker push dockerhub.kubekey.local/karmada/karmada-dashboard-api:node-detail
+docker tag docker.io/karmada/karmada-dashboard-api:membercluster-node-status dockerhub.kubekey.local:9443/karmada/karmada-dashboard-api:node-detail
+docker push dockerhub.kubekey.local:9443/karmada/karmada-dashboard-api:node-detail

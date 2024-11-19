f6ab0761d36d30544c1d80b57613e2c8c0b8aae1
Heylosky <2547226479@qq.com>
2024-11-19 10:18:41 +0800

    function: add get member pods

---
+++ cmd/api/app/routes/member/pod/handler.go
@@ -0,0 +1,35 @@
+package pod
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/karmada-io/dashboard/cmd/api/app/router"
+	"github.com/karmada-io/dashboard/cmd/api/app/types/common"
+	"github.com/karmada-io/dashboard/pkg/client"
+	"github.com/karmada-io/dashboard/pkg/resource/pod"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func handleGetMemberPod(c *gin.Context) {
+	karmadaClient := client.InClusterKarmadaClient()
+	_, err := karmadaClient.ClusterV1alpha1().Clusters().Get(context.TODO(), c.Param("clustername"), metav1.GetOptions{})
+	if err != nil {
+		common.Fail(c, err)
+		return
+	}
+	memberClient := client.InClusterClientForMemberCluster(c.Param("clustername"))
+	dataSelect := common.ParseDataSelectPathParameter(c)
+	nsQuery := common.ParseNamespacePathParameter(c)
+	result, err := pod.GetPodList(memberClient, nsQuery, dataSelect)
+	if err != nil {
+		common.Fail(c, err)
+		return
+	}
+	common.Success(c, result)
+}
+
+func init() {
+	r := router.V1()
+	r.GET("/member/:clustername/pod/:namespace", handleGetMemberPod)
+}
